8133a049b3f03d2a2db12b71bbbd1a429cb5bbe1
Anthony <anthonychinaemeru84@yahoo.com>
2024-10-11 04:48:02 +0100

    Done with Register view conditions

--- app/views.py
+++ app/views.py
@@ -22,6 +22,43 @@ def signup_view(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
         password2 = request.POST.get('password2')
+
+        user_data_has_error = False
+
+        # Validating if the email and the username is being used by another user
+        if User.objects.filter(username=username).exists():
+            user_data_has_error = True
+            messages.error(request, "Username already used")
+
+        if User.objects.filter(email=email).exists():
+            user_data_has_error = True
+            messages.error(request, "Email already used")
+
+        # Checking is passwords matched
+        if password != password2:
+            user_data_has_error = True
+            messages.error(request, "passwords doesn't match")
+
+        # Checking if the users password is not less that 8 characters
+        if len(password) < 8:
+            user_data_has_error = True
+            messages.error(request, "password should not be less than 8 characters ")
+
+        if user_data_has_error:
+            return redirect('register')
+        else:
+            new_user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password,
+                password2=password2
+            )
+            messages.success(request, "Account created successfully")
+            return redirect('login')
+
+        
     return render(request, 'register.html')
 
 def login_view(request):
